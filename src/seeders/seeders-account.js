4bfe496a276ecc4f6978b34fab632944851fe375
vuongoctien <vuongoctien@gmail.com>
2023-10-14 18:29:57 +0700

    vẫn phải để lại bảng account, vì cái role liên quan đến login

---
+++ src/seeders/seeders-account.js
@@ -0,0 +1,436 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        return queryInterface.bulkInsert('accounts', [
+            // {
+            //     nickName: 'doraemon',
+            //     password: "$2a$10$el2VBJ6j8MtorzkpWO8RHeEYltcGKu0hzxT.iw4Cdd6stGy2T4g/y", //vb2tnus
+            //     role: 'Cơ sở Y tế',
+            //     gmail: '@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'o_long_vien',
+            //     password: "$2a$10$el2VBJ6j8MtorzkpWO8RHeEYltcGKu0hzxT.iw4Cdd6stGy2T4g/y", //vb2tnus
+            //     role: 'Cơ sở Y tế',
+            //     gmail: '@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'novagen',
+            //     password: "$2a$10$el2VBJ6j8MtorzkpWO8RHeEYltcGKu0hzxT.iw4Cdd6stGy2T4g/y", //vb2tnus
+            //     role: 'Cơ sở Y tế',
+            //     gmail: '@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'tieu_ngao_giang_ho',
+            //     password: "$2a$10$el2VBJ6j8MtorzkpWO8RHeEYltcGKu0hzxT.iw4Cdd6stGy2T4g/y", //vb2tnus
+            //     role: 'Cơ sở Y tế',
+            //     gmail: '@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'hong_ngoc_cs5',
+            //     password: "$2a$10$el2VBJ6j8MtorzkpWO8RHeEYltcGKu0hzxT.iw4Cdd6stGy2T4g/y", //vb2tnus
+            //     role: 'Cơ sở Y tế',
+            //     gmail: '@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'brain_care',
+            //     password: "$2a$10$el2VBJ6j8MtorzkpWO8RHeEYltcGKu0hzxT.iw4Cdd6stGy2T4g/y", //vb2tnus
+            //     role: 'Cơ sở Y tế',
+            //     gmail: '@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'saigonvietuc',
+            //     password: "$2a$10$el2VBJ6j8MtorzkpWO8RHeEYltcGKu0hzxT.iw4Cdd6stGy2T4g/y", //vb2tnus
+            //     role: 'Cơ sở Y tế',
+            //     gmail: '@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'van_hanh',
+            //     password: "$2a$10$el2VBJ6j8MtorzkpWO8RHeEYltcGKu0hzxT.iw4Cdd6stGy2T4g/y", //vb2tnus
+            //     role: 'Cơ sở Y tế',
+            //     gmail: '@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'chiropractic_q1',
+            //     password: "$2a$10$el2VBJ6j8MtorzkpWO8RHeEYltcGKu0hzxT.iw4Cdd6stGy2T4g/y", //vb2tnus
+            //     role: 'Cơ sở Y tế',
+            //     gmail: '@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'chiropractic_q5',
+            //     password: "$2a$10$el2VBJ6j8MtorzkpWO8RHeEYltcGKu0hzxT.iw4Cdd6stGy2T4g/y", //vb2tnus
+            //     role: 'Cơ sở Y tế',
+            //     gmail: '@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'anh_hung_xa_dieu',
+            //     password: "$2a$10$el2VBJ6j8MtorzkpWO8RHeEYltcGKu0hzxT.iw4Cdd6stGy2T4g/y", //vb2tnus
+            //     role: 'Cơ sở Y tế',
+            //     gmail: '@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'than_dieu_dai_hiep',
+            //     password: "$2a$10$el2VBJ6j8MtorzkpWO8RHeEYltcGKu0hzxT.iw4Cdd6stGy2T4g/y", //vb2tnus
+            //     role: 'Cơ sở Y tế',
+            //     gmail: '@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'y_thien_do_long_ky',
+            //     password: "$2a$10$el2VBJ6j8MtorzkpWO8RHeEYltcGKu0hzxT.iw4Cdd6stGy2T4g/y", //vb2tnus
+            //     role: 'Cơ sở Y tế',
+            //     gmail: '@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'nobita',
+            //     password: '',
+            //     role: 'Bác sĩ',
+            //     gmail: '@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'shizuka',
+            //     password: '',
+            //     role: 'Bác sĩ',
+            //     gmail: '@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'jaian',
+            //     password: '',
+            //     role: 'Bác sĩ',
+            //     gmail: '@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'suneo',
+            //     password: '',
+            //     role: 'Bác sĩ',
+            //     gmail: '@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'olongvien1',
+            //     password: '',
+            //     role: 'Bác sĩ',
+            //     gmail: '@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'olongvien2',
+            //     password: '',
+            //     role: 'Bác sĩ',
+            //     gmail: '@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'olongvien3',
+            //     password: '',
+            //     role: 'Bác sĩ',
+            //     gmail: '@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'olongvien4',
+            //     password: '',
+            //     role: 'Bác sĩ',
+            //     gmail: '@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'lenh_ho_xung',
+            //     password: '',
+            //     role: 'Bác sĩ',
+            //     gmail: '@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'nhac_bat_quan',
+            //     password: '',
+            //     role: 'Bác sĩ',
+            //     gmail: '@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'nhac_linh_san',
+            //     password: '',
+            //     role: 'Bác sĩ',
+            //     gmail: '@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'dpbb',
+            //     password: '',
+            //     role: 'Bác sĩ',
+            //     gmail: '@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'nham_nga_hanh',
+            //     password: '',
+            //     role: 'Bác sĩ',
+            //     gmail: '@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'lam_binh_chi',
+            //     password: '',
+            //     role: 'Bác sĩ',
+            //     gmail: '@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'tham_nguyen',
+            //     password: '',
+            //     role: 'Bác sĩ',
+            //     gmail: '@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'hang_nguyen',
+            //     password: '',
+            //     role: 'Bác sĩ',
+            //     gmail: '@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'hanh_le',
+            //     password: '',
+            //     role: 'Bác sĩ',
+            //     gmail: '@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'khue_dang',
+            //     password: '',
+            //     role: 'Bác sĩ',
+            //     gmail: '@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'van_nguyen',
+            //     password: '',
+            //     role: 'Bác sĩ',
+            //     gmail: '@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'thuy_hoang',
+            //     password: '',
+            //     role: 'Bác sĩ',
+            //     gmail: '@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'loan_tran',
+            //     password: '',
+            //     role: 'Bác sĩ',
+            //     gmail: '@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'an_le',
+            //     password: '',
+            //     role: 'Bác sĩ',
+            //     gmail: '@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'thuy_nguyen',
+            //     password: '',
+            //     role: 'Bác sĩ',
+            //     gmail: '@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'anh_nguyen',
+            //     password: '',
+            //     role: 'Bác sĩ',
+            //     gmail: '@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'nhung_le',
+            //     password: '',
+            //     role: 'Bác sĩ',
+            //     gmail: '@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'nhan_nhieu',
+            //     password: '',
+            //     role: 'Bác sĩ',
+            //     gmail: '@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'benhnhan_real',
+            //     password: "$2a$10$gM4e91FqJpi12r.huvlo0uoPPdNLyBKadRvm2jmOtuMBIgXJYq8fa", //benhnhan
+            //     role: 'Bệnh nhân',
+            //     gmail: 'chuyentoan01@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'benhnhan1',
+            //     password: "$2a$10$gM4e91FqJpi12r.huvlo0uoPPdNLyBKadRvm2jmOtuMBIgXJYq8fa", //benhnhan
+            //     role: 'Bệnh nhân',
+            //     gmail: 'benhnhan1@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'benhnhan2',
+            //     password: "$2a$10$gM4e91FqJpi12r.huvlo0uoPPdNLyBKadRvm2jmOtuMBIgXJYq8fa", //benhnhan
+            //     role: 'Bệnh nhân',
+            //     gmail: 'benhnhan2@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'benhnhan3',
+            //     password: "$2a$10$gM4e91FqJpi12r.huvlo0uoPPdNLyBKadRvm2jmOtuMBIgXJYq8fa", //benhnhan
+            //     role: 'Bệnh nhân',
+            //     gmail: 'benhnhan3@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            // {
+            //     nickName: 'benhnhan4',
+            //     password: "$2a$10$gM4e91FqJpi12r.huvlo0uoPPdNLyBKadRvm2jmOtuMBIgXJYq8fa", //benhnhan
+            //     role: 'Bệnh nhân',
+            //     gmail: 'benhnhan4@gmail.com',
+            //     image: '',
+            //     createdAt: new Date(),
+            //     updatedAt: new Date()
+            // },
+            {
+                nickName: 'admin',
+                password: "$2a$10$el2VBJ6j8MtorzkpWO8RHelyG0FUntajsIM.3mb00TFkcE4UWFs8O", //bookingcare 
+                role: 'Quản trị viên',
+                gmail: '@gmail.com',
+                image: '',
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+        ]);
+    },
+
+    async down(queryInterface, Sequelize) {
+        /**
+         * Add commands to revert seed here.
+         *
+         * Example:
+         * await queryInterface.bulkDelete('People', null, {});
+         */
+    }
+};
+
+/**
+ * password: "$2a$10$3mbpyNnDTHW/cKZ8d/Himu3bm7mFkm0AZZza.uU1LxVFlU9dppx5K", //keckeckec
+ * password: "$2a$10$0EdeN7KzLEM8B/hHHss/IOrAthNr1ljx088uBrcz/OeDqSrYoYTHy", //123456
+ * password: "$2a$10$aC2DSwIf7zfeZVng0x1RwOBNCzFdhTZs5Cr1VYqFcxkTfWf1sGrO.", //tiktok
+ * password: "$2a$10$bRiwC..WvaChopsC.7Fv.OCMTTf2i2bBIoadpfUN7M0PNHFOn60m6", //buiquyen
+ * password: "$2a$10$el2VBJ6j8MtorzkpWO8RHeEEsaZ7XHPDppmgxgLlIryHk6t7U0LX6", //thongminhtube
+
+    
+    
+
+
+ */
